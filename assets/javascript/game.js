2ba559772e27cec2d8bfaeca427435cd02bae0ca
kaylaverheyen <kaylaverheyen@icloud.com>
2018-09-27 13:13:41 -0500

    add gems

---
+++ assets/javascript/game.js
@@ -0,0 +1,87 @@
+//global variables
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
+
+//random number function for the random number, define variable 
+
+$(document).ready(function () {
+    var randomNumber = Math.floor(Math.random() * (120-19) + 19);
+    console.log(randomNumber);
+    $("#random-number").text("RANDOM NUMBER:" + " " + randomNumber);
+});
+//gems or crystals
+var crystalsPoints = [4, 6, 7, 9];
+
+for (var i = 0; i < crystalsPoints.length; i++) {
+
+   // For each iteration, we will create an imageCrystal
+   var imageCrystalRuby = $("<img>");
+   var imageCrystalEmerald = $("<img>");
+   var imageCrystalCarnary = $("<img>");
+   var imageCrystalDiamond = $("<img>");
+
+   // First each crystal will be given the class ".crystal-image".
+   // This will allow the CSS to take effect.
+   imageCrystalRuby.addClass("crystal-image");
+   imageCrystalEmerald.addClass("crystal-image");
+   imageCrystalCarnary.addClass("crystal-image");
+   imageCrystalDiamond.addClass("crystal-image");
+
+
+   // Each imageCrystal will be given a src link to the crystal image
+   imageCrystalRuby.attr("src", "/Users/kaylaverheyen/Desktop/hw_4/unit-4-game/assets/images/gemstone-143303-ruby-oval-red-62043.jpg");
+   imageCrystalEmerald.attr("src", "/Users/kaylaverheyen/Desktop/hw_4/unit-4-game/assets/images/in-detail-gemstone-emerald-06-740x493.jpg"); 
+   imageCrystalCarnary.attr("src", "/Users/kaylaverheyen/Desktop/hw_4/unit-4-game/assets/images/loose-yellow-diamonds-fancy-yellow-diamond-ring-canary-yellow-diamond-fancy-vivid.jpg");
+   imageCrystalDiamond.attr("src", "/Users/kaylaverheyen/Desktop/hw_4/unit-4-game/assets/images/Round-cut-diamond.jpg");
+
+   // Each imageCrystal will be given a data attribute called data-crystalValue.
+   // This data attribute will be set equal to the array value.
+   imageCrystalRuby.attr("data-crystalvalue", crystalsPoints[0]);
+   imageCrystalEmerald.attr("data-crystalvalue", crystalsPoints[1]);
+   imageCrystalCarnary.attr("data-crystalvalue", crystalsPoints[2]);
+   imageCrystalDiamond.attr("data-crystalvalue", crystalsPoints[3]);
+
+   // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+   $("#crystals").append(imageCrystalRuby);
+   $("#crystals").append(imageCrystalEmerald);
+   $("#crystals").append(imageCrystalCarnary);
+   $("#crystals").append(imageCrystalDiamond);
+ }
+
+ // This time, our click event applies to every single crystal on the page. Not just one.
+ $(".crystal-image").on("click", function() {
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    // We then add the crystalValue to the user's "counter" which is a global variable.
+    // Every click, from every crystal adds to the global counter.
+    totalScore += crystalValue;
+    alert("New score: " + totalScore);
+
+    if (totalScore === randomNumber) {
+      alert("You win!");
+    }
+
+    else if (totalScore >= randomNumber) {
+      alert("You lose!!");
+    }
+
+  });
+
+
+
+//game function ie adding crystals equating to random number
+
+    //update total score or (user score)
+    //if total score is equal to random number RETURN WINNER
+        //update wins counter ie wins++
+        //picture of treasure chest appears on click event
+    //if else score is more than > random number RETURN NOT QUITE RIGHT, KEEP TRYING!
+        //update losses counter ie losses++
+        //picture of X out treasure chest appears
+    
+//reset function 
+    //update new random number
+    //keep track of wins and losses
+
+
